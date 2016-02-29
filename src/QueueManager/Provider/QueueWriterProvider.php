e211bacff08bfefc0a15482734b43278498d99ed
Akhoris <akhoris@users.noreply.github.com>
2016-02-29 10:48:33 +0100

    When initializing the ServiceManager Config object, extract the config from the service_manager key

--- src/QueueManager/Provider/QueueWriterProvider.php
+++ src/QueueManager/Provider/QueueWriterProvider.php
@@ -20,9 +20,11 @@ final class QueueWriterProvider
      */
     public static function get(array $config)
     {
+        $serviceConfig = include dirname(dirname(dirname(__DIR__))) . '/config/services.config.php';
+
         $services = new ServiceManager(
             new Config(
-                include dirname(dirname(dirname(__DIR__))) . '/config/services.config.php'
+                $serviceConfig['service_manager']
             )
         );
         $services->setService("Config", $config);
